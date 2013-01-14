4180edb81c7256c2ebedb699dae2e1976541a8b9
Nathan Butler <iamnbutler@gmail.com>
2013-01-13 19:57:06 -0500

    Minor splash page updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 // Load scripts after page loads
-$(document).ready(function(){
+$(window).load(function(){
 	// Logo animations
 	$('#logo-target').fadeIn(1600);
 	$('#headline').delay(2000).fadeIn(800);
 	$('#subhead').delay(3000).fadeIn(400);
 
 	// Coming soon animation
-	$('#coming-soon-target').delay(4000).fadeIn(800);
+	$('#coming-soon-target').delay(3600).fadeIn(800);
 
 	// Social animations
-	$('#behance-target').delay(5000).fadeIn(800);
-	$('#dribbble-target').delay(5000).fadeIn(800);
-	$('#twitter-target').delay(6000).fadeIn(800);
-	$('#github-target').delay(6000).fadeIn(800);
+	$('#behance-target').delay(4800).fadeIn(800);
+	$('#dribbble-target').delay(4800).fadeIn(800);
+	$('#twitter-target').delay(5800).fadeIn(800);
+	$('#github-target').delay(5800).fadeIn(800);
 });
